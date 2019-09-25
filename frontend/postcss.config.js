89959f62a0aba5de73ef1d17586f88b08bc09a7d
Jonas Niestroj <j.niestroj@protonmail.ch>
2019-09-25 13:14:14 +0200

    Initial Vue setup

---
+++ frontend/postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    require('postcss-import')(),
+    require('tailwindcss')(),
+    require('postcss-preset-env')({
+      features: {
+        'nesting-rules': true,
+      },
+    }),
+    require('autoprefixer')(),
+  ],
+}
---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <header class="py-6 bg-gray-200">
+      <div class="container">
+        <div class="flex justify-center">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>
+    </header>
+
+    <main class="mt-12">
+      <router-view />
+    </main>
+  </div>
+</template>
