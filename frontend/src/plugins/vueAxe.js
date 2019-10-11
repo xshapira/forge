90ae622c2b43d568fbe9dfd57e54dcef6032ea4a
ingvijonasson <contact@ingvijonasson.com>
2019-10-11 08:22:33 +0200

    Remve extra configuration from vue-axe

--- frontend/src/plugins/vueAxe.js
+++ frontend/src/plugins/vueAxe.js
@@ -10,17 +10,6 @@ if (process.env.NODE_ENV !== 'production') {
   Vue.use(VueAxe, {
     // clears the console after render
     clearConsoleOnUpdate: false,
-
-    config: {
-      rules: [
-        { id: 'color-contrast', enabled: true },
-        { id: 'heading-order', enabled: true },
-        { id: 'help-same-as-label', enabled: true },
-        { id: 'label-title-only', enabled: true },
-        { id: 'link-in-text-block', enabled: true },
-        { id: 'region', enabled: true },
-        { id: 'skip-link', enabled: true },
-      ],
-    },
+    config: {},
   })
 }
