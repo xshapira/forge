37f2acd455cfe3abbd80336eb78665ba54f25924
ingvijonasson <contact@ingvijonasson.com>
2019-10-11 11:35:10 +0200

    Introduce toggle component WIP

---
+++ frontend/src/components/Toggle.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="toggle">
+    <button
+      class="toggle__btn"
+      :class="[{ 'block w-full border-b bg-blue-200': isBlock }, classButton]"
+      @click="toggle"
+    >
+      <slot></slot>
+    </button>
+    <aside class="toggle__content" :class="{ hidden: isHidden }">
+      <slot name="content"></slot>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isBlock: {
+      type: Boolean,
+      default: false,
+    },
+    classButton: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      isHidden: true,
+    }
+  },
+  methods: {
+    toggle() {
+      this.isHidden = !this.isHidden
+    },
+  },
+}
+</script>
