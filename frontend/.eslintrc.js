a8299193752da686988f3d71a28fe42dd5a10157
Stefan Heinemann <stefan.heinemann@liip.ch>
2019-10-25 18:06:45 +0200

    Disable component name casing rule for vue

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
+  rules: {
+    "vue/component-name-in-template-casing": 0
+  },
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)'],
