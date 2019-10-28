5dd3b4fd4797fcb459d5df82e59e8f57af9909e5
ingvijonasson <contact@ingvijonasson.com>
2019-10-28 16:00:09 +0100

    Fix prettier issue with code-example component

--- frontend/src/styleguide/CodeExample.vue
+++ frontend/src/styleguide/CodeExample.vue
@@ -1,8 +1,9 @@
 <template>
   <pre
-    class="w-2/3 mt-2 py-1 px-2 text-sm whitespace-normal rounded border bg-gray-100"
-    >{{ code }}</pre
+    class="mt-2 py-1 px-2 text-sm whitespace-normal border rounded bg-gray-100"
   >
+    {{ code }}
+  </pre>
 </template>
 
 <script>
