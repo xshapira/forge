f9beff2287571effbdb670b844dbaca0fdf5b1e3
Raphi <raphael.antonietti@liip.ch>
2019-11-06 17:50:50 +0100

    Add focus to modal content instead

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -12,12 +12,11 @@ TODO:
 <template>
   <div
     v-if="isOpen"
-    v-focus
     :aria-hidden="isOpen ? 'false' : 'true'"
     @keydown.esc="hideModal"
   >
     <div class="a11y-dialog-backdrop" :class="backdropClasses"></div>
-    <div class="a11y-dialog relative z-10">
+    <div v-focus class="a11y-dialog relative z-10">
       <div class="a11y-dialog--header">
         <slot name="modal-head" />
       </div>
