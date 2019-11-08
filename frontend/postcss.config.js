c2cf9e37b8823930ae3180212791cf417e067607
ingvijonasson <contact@ingvijonasson.com>
2019-11-08 14:25:44 +0100

    Add ie11 support for custom properties

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -3,6 +3,8 @@ module.exports = {
     require('postcss-import')(),
     require('tailwindcss')(),
     require('postcss-preset-env')({
+      browsers: 'last 2 versions',
+      stage: 3,
       features: {
         'nesting-rules': true,
       },
