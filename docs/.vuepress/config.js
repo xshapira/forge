07dca4465469927d6168aa0013a126d70b3c9139
ingvijonasson <contact@ingvijonasson.com>
2019-11-08 15:13:48 +0100

    Setup documentation sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,36 @@ module.exports = {
   title: 'Forge',
   description: 'The documentation for Forge template',
   themeConfig: {
-    displayAllHeaders: true,
-    sidebar: 'auto'
+    smoothScroll: true,
+    lastUpdated: 'Last Updated',
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/liip-amboss/forge' },
+    ],
+    sidebar: [{
+      title: 'Frontend',
+      collapsable: false,
+      children: [
+        '/frontend/',
+        '/frontend/accessibilty',
+        '/frontend/svg',
+        {
+          title: "Components",
+          collapsable: false,
+          children: [
+            '/frontend/components/input',
+            '/frontend/components/popover',
+            '/frontend/components/toggle',
+            '/frontend/components/tooltip'
+          ]
+        }
+      ]
+    },
+    {
+      title: "Backend",
+      collapsable: false,
+      children: [
+        '/backend/',
+      ]
+    }],
   }
 }
