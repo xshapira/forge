777985f72169b35f15f28cf50d3808f67b6143fa
Raphi <raphael.antonietti@liip.ch>
2019-12-11 09:04:08 +0100

    Add old configs again

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -28,6 +28,8 @@ module.exports = {
     require('autoprefixer'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
     require('postcss-preset-env')({
+      browsers: 'last 2 versions',
+      stage: 3,
       features: {
         'nesting-rules': true,
       },
