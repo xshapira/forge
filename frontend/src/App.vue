af3ec12dd3b90b32c98fde326af7764f0e8ce998
ingvijonasson <contact@ingvijonasson.com>
2020-01-03 15:57:40 +0100

    Introduce sticky footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <header class="py-6 bg-gray-100">
-      <div class="container">
-        <div class="flex justify-between">
-          <router-link class="no-underline font-semibold text-lg" to="/">
-            Forge
-          </router-link>
-          <router-link
-            class="no-underline font-semibold text-lg"
-            to="/styleguide"
-          >
-            Styleguide
-          </router-link>
+  <div id="app" class="app-master">
+    <div class="app-content">
+      <header class="py-6 bg-gray-100">
+        <div class="container">
+          <div class="flex justify-between">
+            <router-link class="text-lg font-semibold no-underline" to="/">
+              Forge
+            </router-link>
+            <router-link
+              class="text-lg font-semibold no-underline"
+              to="/styleguide"
+            >
+              Styleguide
+            </router-link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <main>
+        <router-view />
+      </main>
+    </div>
 
-    <main>
-      <router-view />
-    </main>
-    <footer>
-      &nbsp;
+    <footer class="py-6 bg-gray-100 app-footer">
+      <div class="container">
+        <p class="text-lg font-semibold">App footer</p>
+      </div>
     </footer>
 
     <portal-target name="body-end" />
   </div>
 </template>
+
+<style>
+.app-master {
+  min-height: 100%;
+  display: grid;
+  grid-template-rows: 1fr auto;
+}
+.app-footer {
+  grid-row-start: 2;
+  grid-row-end: 3;
+}
+</style>
