05611343023b0991ceda71e91f0160b867426ba7
Jonas Niestroj <j.niestroj@protonmail.ch>
2020-01-08 11:41:30 +0100

    Added comment to notification example

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -34,6 +34,8 @@ export default {
 <div>
 <button class="btn" @click="success">Show success</button>
 <button class="btn" @click="error">Show error</button>
+
+<!-- This component should be globally -->
 <notification />
 </div>
 </template>
