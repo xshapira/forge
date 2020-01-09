29dcd9d478b5a313d85cf3757863d805884d0060
Jonas Niestroj <j.niestroj@protonmail.ch>
2020-01-09 08:52:25 +0100

    Moved example to own file
    Moved the notification example to a own vue file to work with prettier and prevent eslint errors

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -28,28 +28,6 @@ export default {
 </script>
 
 <docs>
-```vue
-<template>
-  <div>
-    <button class="btn" @click="success">Show success</button>
-    <button class="btn" @click="error">Show error</button>
-
-    <!-- This component should be globally -->
-    <notification />
-  </div>
-</template>
-
-<script>
-export default {
-	methods: {
-		success() {
-			this.$notify({text: 'Test'});
-		},
-		error() {
-			this.$notify({ text: 'Error', type: 'error' });
-		}
-	}
-}
-</script>
+```[import](../examples/NotificationExample.vue)
 ```
 </docs>
