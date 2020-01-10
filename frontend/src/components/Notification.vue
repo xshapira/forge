30c119681647f722352eab4ffde463f1244e245c
Jonas Niestroj <j.niestroj@protonmail.ch>
2020-01-10 07:21:30 +0100

    Added currentColor & errortype constant

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -2,7 +2,11 @@
   <notifications position="top center" class="min-w-128">
     <template slot="body" slot-scope="props">
       <div class="notification" :class="getClass(props.item.type)">
-        <svg-icon :name="getIcon(props.item.type)" class="icon-md mr-3 mt-1" />
+        <svg-icon
+          :name="getIcon(props.item.type)"
+          class="icon-md mr-3 mt-1"
+          :class="getColorClass(props.item.type)"
+        />
         <div class="flex-1">
           <span v-if="props.item.text">
             {{ props.item.text }}
@@ -15,13 +19,18 @@
 </template>
 
 <script>
+const ERRORTYPE = 'error';
+
 export default {
   methods: {
     getIcon(type) {
-      return type === 'error' ? 'warning-circle' : 'done-circle';
+      return type === ERRORTYPE ? 'warning-circle' : 'done-circle';
     },
     getClass(type) {
-      return type === 'error' ? 'is-error' : 'is-success';
+      return type === ERRORTYPE ? 'is-error' : 'is-success';
+    },
+    getColorClass(type) {
+      return type === ERRORTYPE ? 'text-red-600' : 'text-blue-500';
     },
   },
 };
