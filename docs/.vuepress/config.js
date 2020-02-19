314dc889d9d67d75dffeb1e9775829e7bc85a611
Jonas Niestroj <j.niestroj@protonmail.ch>
2020-02-19 14:37:26 +0100

    Added select section to docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,37 +1,38 @@
 module.exports = {
-  title: 'Forge',
-  description: 'The documentation for Forge template',
+  title: "Forge",
+  description: "The documentation for Forge template",
   themeConfig: {
     smoothScroll: true,
-    lastUpdated: 'Last Updated',
-    nav: [{ text: 'GitHub', link: 'https://github.com/liip-amboss/forge' }],
+    lastUpdated: "Last Updated",
+    nav: [{ text: "GitHub", link: "https://github.com/liip-amboss/forge" }],
     sidebar: [
       {
-        title: 'Frontend',
+        title: "Frontend",
         collapsable: false,
         children: [
-          '/frontend/',
-          '/frontend/accessibilty',
-          '/frontend/linting',
-          '/frontend/purge',
-          '/frontend/SVG',
-          '/frontend/testing',
-          '/frontend/vuelidate',
+          "/frontend/",
+          "/frontend/accessibilty",
+          "/frontend/linting",
+          "/frontend/purge",
+          "/frontend/SVG",
+          "/frontend/testing",
+          "/frontend/vuelidate",
           {
-            title: 'Components',
+            title: "Components",
             collapsable: false,
             children: [
-              '/frontend/components/modal',
-              '/frontend/components/simple-table',
-              '/frontend/components/notification'
+              "/frontend/components/modal",
+              "/frontend/components/simple-table",
+              "/frontend/components/notification",
+              "/frontend/components/select"
             ]
           }
         ]
       },
       {
-        title: 'Backend',
+        title: "Backend",
         collapsable: false,
-        children: ['/backend/']
+        children: ["/backend/"]
       }
     ]
   }
