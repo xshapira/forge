6cc941ba1837a2f783d2a9625620c4789d92a66a
Jonas Niestroj <j.niestroj@protonmail.ch>
2020-02-24 08:09:44 +0100

    Added i18n to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -23,10 +23,14 @@ import './plugins/notifications';
 // Global components
 import './globals/svgIcon';
 
+// I18n
+import i18n from './i18n';
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App),
 }).$mount('#app');
