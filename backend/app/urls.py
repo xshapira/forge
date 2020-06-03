99566b791a4614d89b14c91b187be3925527c8f8
Adrian Berger <adrian.berger@liip.ch>
2020-06-03 13:41:26 +0200

    feat: add default path to urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from django.urls import include
+from django.urls import include, re_path
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -24,6 +25,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(),
          name='token_refresh'),
     path('admin/', admin.site.urls),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
