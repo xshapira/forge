8ee16d864619893f19204296c9c06a385fd9153e
Adrian Berger <adrian.berger@liip.ch>
2020-06-05 15:54:21 +0200

    fix: remove eslint errors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
-import store from "@/store/";
+import store from '@/store/';
 
 const mtgApi = axios.create({
   baseURL: `https://api.scryfall.com`,
 });
 
 const loginInterceptor = config => {
-  if (store.getters["Auth/isLoggedIn"]) {
+  if (store.getters['Auth/isLoggedIn']) {
     const token = store.state.Auth.token;
-    config.headers.common["Authorization"] = `Bearer ${token}`;
+    config.headers.common['Authorization'] = `Bearer ${token}`;
   }
 
   return config;
 };
 
 const userApi = axios.create({
-  baseURL: `${process.env.VUE_APP_API_URL}`
+  baseURL: `${process.env.VUE_APP_API_URL}`,
 });
 
 userApi.interceptors.request.use(loginInterceptor);
