bde16533f75fc46a3e4f9517d364dd519a7e93c3
Adrian Berger <adrian.berger@liip.ch>
2020-06-16 09:33:13 +0200

    fix: use email for user creation form in backend

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -11,7 +11,11 @@ class UserAdmin(BaseUserAdmin):
     ordering = ('email',)
     list_display = ('email', 'first_name', 'last_name', 'is_active')
     search_fields = ['first_name', 'last_name', 'email']
-
+    add_fieldsets = (
+        ('Account', {
+            'fields': ('email', 'password1', 'password2',)
+        }),
+    )
     fieldsets = (
         ('Account', {
             'fields': ('email', 'password', 'first_name', 'last_name')
